b7e19beec20bea4d1491f362222a37c655387d4e
tina <tinhinaneachir@gmail.com>
2025-06-08 00:10:26 +0200

    add service user details

---
+++ eventhub-backend/src/main/java/com/eventhub/eventhub_backend/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.eventhub.eventhub_backend.service;
+
+
+import com.eventhub.eventhub_backend.model.User;
+import com.eventhub.eventhub_backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassworld())
+                .roles(user.getRole().toString())
+                .build();
+    }
+}
