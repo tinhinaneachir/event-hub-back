09eaff92fc8c1276add1cc9f185a0947b24b02e0
tina <tinhinaneachir@gmail.com>
2025-06-15 23:40:29 +0200

    add user controller

--- eventhub-backend/src/main/java/com/eventhub/eventhub_backend/controller/UserController.java
+++ eventhub-backend/src/main/java/com/eventhub/eventhub_backend/controller/UserController.java
@@ -1,4 +1,45 @@
 package com.eventhub.eventhub_backend.controller;
 
+
+import com.eventhub.eventhub_backend.model.User;
+import com.eventhub.eventhub_backend.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserRepository userRepository;
+
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> getCurrentUSer(Principal principal){
+        return  userRepository.findByEmail(principal.getName())
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUser(User user){
+        return ResponseEntity.ok(userRepository.findAll());
+    }
+
+    @DeleteMapping("{/id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id, Principal principal){
+        if(userRepository.existsById(id)){
+            userRepository.deleteById(id);
+            return ResponseEntity.ok("Utilisateur supprimé");
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
+
